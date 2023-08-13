1867867a9815c5701ada61be8dca7fd74105c12f
qasim-leo <qasim@jfreaks.com>
2023-08-13 17:27:39 +0500

    Exception Handling

--- 04-Spring Boot REST Crud/01-rest-crud/src/main/java/com/codingislove/PGPD/rest/StudentRestController.java
+++ 04-Spring Boot REST Crud/01-rest-crud/src/main/java/com/codingislove/PGPD/rest/StudentRestController.java
@@ -45,6 +45,7 @@ public class StudentRestController {
         return theStudent.get(studentId);
     }
 
+    /*
     // Add an exception handler using ExceptionHandler
     @ExceptionHandler
     public ResponseEntity<StudentErrorResponse> handleException (StudentNotFoundException exc){
@@ -58,4 +59,19 @@ public class StudentRestController {
         // return responsibility
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
+
+    // Add another exception handler
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse>  handleException(Exception exc){
+
+        // create a StudentErrorResponse
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus((HttpStatus.BAD_REQUEST.value()));
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        // return responsibility
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+     */
 }
