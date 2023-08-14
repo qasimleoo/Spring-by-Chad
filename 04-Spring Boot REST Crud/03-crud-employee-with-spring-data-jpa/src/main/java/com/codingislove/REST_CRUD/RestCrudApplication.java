80ce3f7d9621323c77227f8e2d17dc7ab7e04514
qasim-leo <qasim@jfreaks.com>
2023-08-14 18:38:22 +0500

    Spring Data JPA

---
+++ 04-Spring Boot REST Crud/03-crud-employee-with-spring-data-jpa/src/main/java/com/codingislove/REST_CRUD/RestCrudApplication.java
@@ -0,0 +1,13 @@
+package com.codingislove.REST_CRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestCrudApplication.class, args);
+	}
+
+}
