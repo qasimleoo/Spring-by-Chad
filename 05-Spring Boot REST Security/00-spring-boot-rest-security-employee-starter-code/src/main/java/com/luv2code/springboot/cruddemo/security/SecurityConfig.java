9d0a467d3d6c15e8067b5b40b0609d53cfb99330
qasim-leo <qasim@jfreaks.com>
2023-08-15 11:59:15 +0500

    Security

---
+++ 05-Spring Boot REST Security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/SecurityConfig.java
@@ -0,0 +1,70 @@
+package com.luv2code.springboot.cruddemo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SecurityConfig {
+
+    // add support for JDBC ... no more hard coded users --
+    @Bean
+    public UserDetailsManager userDetailsManager (DataSource dataSource){
+        return new JdbcUserDetailsManager((dataSource));
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+
+        httpSecurity.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+        );
+
+        // use http basic authentication
+        httpSecurity.httpBasic(Customizer.withDefaults());
+
+        // disable csrf (Cross Site request forgery)
+        // not required for this CRUD or patch but still
+        httpSecurity.csrf(csrf -> csrf.disable());
+
+        return httpSecurity.build();
+    }
+        /*
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+
+        UserDetails john = User.builder()
+                .username("john")
+                .password("{noop}test123")
+                .roles("EMPLOYEE")
+                .build();
+        UserDetails marry = User.builder()
+                .username("marry")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+        UserDetails susan = User.builder()
+                .username("susan")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(john, marry, susan);
+    }
+    */
+}
